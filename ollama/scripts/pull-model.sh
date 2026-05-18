67fcef0b6f1482a9227368cfc285c369aad5c5c2
warungbudina-digital <warungbudina@gmail.com>
2026-05-18 02:07:22 +0000

    Add Ollama pull script skeleton

---
+++ ollama/scripts/pull-model.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# phase-1 model pull skeleton
+# intended responsibilities:
+# - pull exactly one required model
+# - avoid fetching extra models
+
+set -eu
+
+echo "pull-model skeleton"
+exit 0
